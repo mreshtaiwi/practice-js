33950a847c47183500785e9236b3f664e5c9a67c
mreshtaiwi <mr.eshtaiwi@gmail.com>
2020-09-22 17:36:02 +0300

    add js

--- js/add-content.js
+++ js/add-content.js
@@ -1,14 +1,29 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// var today = new Date();
 
-if (hourNow > 18) {
+// console.log(today);
+// var hourNow = today.getHours();
+// console.log(hourNow);
+var hourNow = prompt('please enter an hour from 0-23');
+var greeting;
+console.log(greeting);
+
+if (hourNow > 18 && hourNow <= 23) {
     greeting = 'Good evening, Class!';
-  } else if (hourNow > 12) {
+  } else if (hourNow > 12 && hourNow <= 18) {
     greeting = 'Good afternoon, Class!';
-  } else if (hourNow >= 0) {
+  } else if (hourNow >= 0 && hourNow <= 12) {
     greeting = 'Good morning!';
   } else {
     greeting = 'Something went wrong!';
   }
+  
+ var age = 10;
+ age = age + 10;
+ console.log(age);
+
+ var fname = 'shihab ';
+ var lname = 'eshtaiwi';
+
+ console.log(fname+lname);
+
 document.write('<h3>'+greeting+'</h3>');
